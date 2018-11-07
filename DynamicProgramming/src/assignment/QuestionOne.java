2b91e0d845aded23a0269a04cc5869f5f2fefdfc
Alex-Alexiev <aalexiev@bayviewglen.ca>
2018-11-07 01:21:09 -0500

    thought I did q1 wrong but i just can't read...

--- DynamicProgramming/src/assignment/QuestionOne.java
+++ DynamicProgramming/src/assignment/QuestionOne.java
@@ -4,10 +4,17 @@ public class QuestionOne{
 	
     public static void main(String[] args) {
     	
-    	int[] sequence = { 1, 17, 5, 10, 13, 15, 10, 5, 16, 8 };
+    	int[] sequence = { 1, 7, 4, 9, 2, 5 };
     	
     	int[] solutions = new int[sequence.length];
     	solutions[0] = 1;
+    	
+    	/*
+    	 * keep track of the longest sequence up to the given index in an array
+    	 * If the current term is part of a zig zag sequence, then the longest sequence up to that term is 
+    	 * equal to the longest sequence up to the previous term + 1
+    	 * Otherwise the longest sequence up to that term is equal to the longest sequence up to the previous term
+    	 */
     	for (int i = 1; i < sequence.length-1; i++) {
     		if ((sequence[i]-sequence[i-1])*(sequence[i+1]-sequence[i]) < 0) {        
     			solutions[i] = solutions[i-1]+1;
--- DynamicProgramming/src/assignment/QuestionTwo.java
+++ DynamicProgramming/src/assignment/QuestionTwo.java
@@ -6,6 +6,9 @@ public class QuestionTwo {
 		
 		int[] vals = { 1, 2, 3, 4, 5, 1, 2, 3, 4, 5 };
 		
+		//start once from the first house up until the second last, then start again from the second house
+		//up until the last
+		//then get the bigger value out of those two, since the first and last house can never be together 
 		System.out.println(Math.max(getBest(vals, 0), getBest(vals, 1)));
 	}
 
@@ -19,31 +22,36 @@ public class QuestionTwo {
 		//otherwise we start at index 1 so check the last index
 		//this is because the first and last index can never be in the same donation pilie
 		int checkLast = (startIndex == 0) ? 1 : 0;
-		/*
-		 * essentially go through the 
-		 */
+		
 		for (int i = startIndex; i < vals.length - checkLast; i++) {
 			int prevI = (i < 1) ? 0 : i - 1;
+			//If the previous house is unused, then the best possible donation at the current spot
+			//is that at the previous spot + the current spot value
 			if (used[prevI] == 0) {
 				best[i] = best[prevI] + vals[i];
 				used[i] = 1;
 			}
-			// previous one is used
+			//if the previous house is being used and we are past the second house
 			else if (i > 1) {
-				// bigger than previous one
-				if (vals[i] > vals[i-1] || vals[i] + best[i-2] > best[i-1]) {
+				//if the current value plus the best value up until the house 2 previous to current is bigger
+				//than the max donation from the direct neighbor, then 
+				//set the current best to the best at i-2 + the current value
+				//set the previous neighbor to being unused
+				//set the current house to being used
+				if (vals[i] + best[i-2] > best[i-1]) {
 					best[i] = best[i-2] + vals[i];
-					used[i-2] = 1;
-					used[i-2] = 0;
+					used[i-1] = 0;
 					used[i] = 1;
 				}
 				else {
 					best[i] = best[i-1];
 					used[i] = 0;
 				}
+			//otherwise if we are just checking the first two houses, use the bigger value 
 			} else {
 				if (vals[i] > vals[i - 1]) {
 					best[i] = vals[i];
+					used[i] = 1;
 				} else {
 					best[i] = best[i-1];
 					used[i] = 0;
