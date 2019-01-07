61f5c5ca747fdf8202e2e85a70eef304c99847a1
Alexander Alexiev <aalexiev@bayviewglen.ca>
2019-01-06 19:13:05 -0500

    smore work on horsies

--- HorseRacing/js/script.js
+++ HorseRacing/js/script.js
@@ -1,10 +1,23 @@
 $(function () {
 
     var START_BALANCE = 500;
-    var players = [];
+    var players = [{
+            name: "benbet",
+            balance: 500
+        },
+        {
+            name: "clarkbet",
+            balance: 500
+        }
+    ];
+
+    updateTable();
 
     function addPlayer() {
-        players.push({ name: $("#nameInput").val(), balance: START_BALANCE });
+        players.push({
+            name: $("#nameInput").val(),
+            balance: START_BALANCE
+        });
     }
 
     userEntry = $("#newPlayerForm").dialog({
@@ -13,8 +26,14 @@ $(function () {
         width: 350,
         modal: true,
         buttons: {
-            "Create Player": function () { addPlayer(); updateTable(); userEntry.dialog("close"); },
-            Cancel: function () { userEntry.dialog("close") }
+            "Create Player": function () {
+                addPlayer();
+                updateTable();
+                userEntry.dialog("close");
+            },
+            Cancel: function () {
+                userEntry.dialog("close")
+            }
         }
     });
 
@@ -46,14 +65,14 @@ $(function () {
 
     var horses = ["clark", "ben", "jon"];
 
-    function updateBetForm(){
+    function updateBetForm() {
         $("#playerSelect").empty();
-        $.each(players, function(i,p){
+        $.each(players, function (i, p) {
             let name = players[i].name;
             $("#playerSelect").append($("<option></option>").val(name).html(name));
         });
         $("#horseSelect").empty();
-        $.each(horses, function(i, p){
+        $.each(horses, function (i, p) {
             $("#horseSelect").append($("<option></option>").val(p).html(p));
         });
     }
@@ -66,32 +85,63 @@ $(function () {
 
     $("#makeBet").button().on("click", makeBet)
 
-    function makeBet(){
-        bets.push({name:$("#playerSelect").val(), horse:$("#horseSelect").val(), amount:$("#betAmount").val()});
+    function makeBet() {
+        bets.push({
+            name: $("#playerSelect").val(),
+            horse: $("#horseSelect").val(),
+            amount: parseInt($("#betAmount").val())
+        });
+        for (let i = 0; i < players.length; i++) {
+            if (players[i].name == $("#playerSelect").val()) {
+                players[i].balance -= parseInt($("#betAmount").val());
+            }
+        }
     }
 
-    $("#watchRace").button().on("click", function(){
+    $("#watchRace").button().on("click", function () {
         $("#placeBetsDiv").hide(1000);
         $("#watchRaceDiv").show(1000);
         race();
     });
 
-    function race(){
-        var winner = horses[Math.floor(Math.random()*horses.length)];
-        $("#winnerText").append(document.createTextNode(winner+"!"))
+    var winningHorse;
+
+    function race() {
+        winningHorse = horses[Math.floor(Math.random() * horses.length)];
+        $("#winnerText").append(document.createTextNode(winningHorse + "!"))
     }
 
-    $("#showResults").button().on("click", function(){
+    $("#showResults").button().on("click", function () {
+        calculateResults();
         $("#watchRaceDiv").hide(1000);
-        $("#showResultsDiv").show(1000);
+        $("#addPlayersDiv").show(1000);
+        updateTable();
     });
 
-    $("#playAgain").button().on("click", function(){
+    function calculateResults() {
+        var winningPlayers = [];
+        var totalBets = 0;
+        for (let i = 0; i < bets.length; i++) {
+            totalBets += bets[i].amount;
+            if (bets[i].horse == winningHorse) {
+                winningPlayers.push(bets[i].name);
+            }
+        }
+        if (winningPlayers.length > 0) {
+            var winningsPerPlayer = totalBets / (winningPlayers.length);
+            for (let i = 0; i < winningPlayers.length; i++) {
+                for (let j = 0; j < players.length; j++) {
+                    if (players[j].name == winningPlayers[i]) {
+                        players[j].balance += winningsPerPlayer;
+                    }
+                }
+            }
+        }
+    }
+
+    $("#playAgain").button().on("click", function () {
         $("#showResultsDiv").hide(1000);
         $("#addPlayersDiv").show(1000);
     });
 
 });
-
-
-
