275df3bbd0abcea4279730dc7786fa9ca6f6b771
Alex-Alexiev <aalexiev@bayviewglen.ca>
2019-03-06 01:05:02 -0500

    shakey errror

--- MEVN/contactList/client/src/components/EditContact.vue
+++ MEVN/contactList/client/src/components/EditContact.vue
@@ -3,16 +3,17 @@
     <h1>Edit Contact</h1>
     <div class="form">
       <div>
-        <input type="text" name="firstName" placeholder="FIRST NAME" v-model="firstName">
+        <input type="text" name="firstName" v-bind:class="{error: this.errors[0]}" placeholder="FIRST NAME" v-model="firstName">
       </div>
       <div>
-        <input type="text" name="lastName" placeholder="LAST NAME" v-model="lastName">
+        <input type="text" name="lastName" v-bind:class="{error: this.errors[1]}" placeholder="LAST NAME" v-model="lastName">
       </div>
       <div>
-        <input type="text" name="phoneNumber" placeholder="XXX-YYY-ZZZZ" v-model="phoneNumber">
+        <input type="text" name="phoneNumber" v-bind:class="{error: this.errors[2]}" placeholder="XXX-YYY-ZZZZ" v-model="phoneNumber">
       </div>
       <div>
         <input
+        v-bind:class="{error: this.errors[3]}"
           type="text"
           name="streetAddress"
           placeholder="STREET ADDRESS"
@@ -20,10 +21,10 @@
         >
       </div>
       <div>
-        <input type="text" name="city" placeholder="CITY" v-model="city">
+        <input type="text" name="city" v-bind:class="{error: this.errors[4]}" placeholder="CITY" v-model="city">
       </div>
       <div>
-        <select name="provinceCode" v-model="provinceCode">
+        <select name="provinceCode" v-bind:class="{error: this.errors[5]}" v-model="provinceCode">
           <option value="NL">NL</option>
           <option value="PE">PE</option>
           <option value="NS">NS</option>
@@ -40,10 +41,10 @@
         </select>
       </div>
       <div>
-        <input type="text" name="postalCode" placeholder="POSTAL CODE" v-model="postalCode">
+        <input type="text" name="postalCode" v-bind:class="{error: this.errors[6]}" placeholder="POSTAL CODE"  v-model="postalCode">
       </div>
       <div>
-        <input type="text" name="email" placeholder="EMAIL" v-model="email">
+        <input type="text" name="email" v-bind:class="{error: this.errors[7]}" placeholder="EMAIL" v-model="email">
       </div>
       <div>
         <button class="app_contact_btn" @click="updateContact">Update Contact</button>
@@ -66,7 +67,8 @@ export default {
       city: "",
       provinceCode: "",
       postalCode: "",
-      email: ""
+      email: "",
+      errors: [false, false, false, false, false, false, false, false]
     };
   },
   mounted() {
@@ -79,36 +81,52 @@ export default {
       var regPostalCode = /^[A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\d$/i;
       var regEmail = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/i;
       if (this.firstName.length <= 0) {
-        alert("first name");
+       this.errors[0] = true;
         valid = false;
+      }else {
+        this.errors[0] = false;
       }
       if (this.lastName.length <= 0) {
-        alert("last name");
+        this.errors[1] = true;
         valid = false;
+      }else {
+        this.errors[1] = false;
       }
       if (!regPhone.test(this.phoneNumber)) {
-        alert("bad phone");
+        this.errors[2] = true;
         valid = false;
+      }else {
+        this.errors[2] = false;
       }
       if (this.streetAddress.length <= 0) {
-        alert("address");
+        this.errors[3] = true;
         valid = false;
+      }else {
+        this.errors[3] = false;
       }
       if (this.city.length <= 0) {
-        alert("city");
+        this.errors[4] = true;
         valid = false;
+      }else {
+        this.errors[4] = false;
       }
       if (this.provinceCode.length <= 0) {
-        alert("province");
+        this.errors[5] = true;
         valid = false;
+      }else {
+        this.errors[5] = false;
       }
       if (!regPostalCode.test(this.postalCode)) {
-        alert("bad postal code");
+        this.errors[6] = true;
         valid = false;
+      } else {
+        this.errors[6] = false;
       }
       if (!regEmail.test(this.email)) {
-        alert("bad email");
+        this.errors[7]
         valid = false;
+      }else {
+        this.errors[7] = false;
       }
       return valid;
     },
@@ -139,6 +157,8 @@ export default {
           email: this.email
         });
         this.$router.push({ name: "Contacts" });
+      } else {
+        this.$forceUpdate();
       }
     }
   }
@@ -156,6 +176,30 @@ export default {
 }
 .form div {
   margin: 20px;
+}
+.error {
+  background: palevioletred;
+  animation: shake 0.82s cubic-bezier(.36,.07,.19,.97) both;
+  transform: translate3d(0, 0, 0);
+  backface-visibility: hidden;
+  perspective: 1000px;
+}
+@keyframes shake {
+  10%, 90% {
+    transform: translate3d(-1px, 0, 0);
+  }
+  
+  20%, 80% {
+    transform: translate3d(2px, 0, 0);
+  }
+
+  30%, 50%, 70% {
+    transform: translate3d(-4px, 0, 0);
+  }
+
+  40%, 60% {
+    transform: translate3d(4px, 0, 0);
+  }
 }
 .app_contact_btn {
   background: #4d7ef7;
--- MEVN/contactList/client/src/components/NewContact.vue
+++ MEVN/contactList/client/src/components/NewContact.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="contacts">
-    <h1>Add Contact</h1>
+    <h1>New Contact</h1>
     <div class="form">
       <div>
-        <input type="text" name="firstName" placeholder="FIRST NAME" v-model="firstName">
+        <input type="text" name="firstName" v-bind:class="{error: this.errors[0]}" placeholder="FIRST NAME" v-model="firstName">
       </div>
       <div>
-        <input type="text" name="lastName" placeholder="LAST NAME" v-model="lastName">
+        <input type="text" name="lastName" v-bind:class="{error: this.errors[1]}" placeholder="LAST NAME" v-model="lastName">
       </div>
       <div>
-        <input type="text" name="phoneNumber" placeholder="XXX-YYY-ZZZZ" v-model="phoneNumber">
+        <input type="text" name="phoneNumber" v-bind:class="{error: this.errors[2]}" placeholder="XXX-YYY-ZZZZ" v-model="phoneNumber">
       </div>
       <div>
         <input
+        v-bind:class="{error: this.errors[3]}"
           type="text"
           name="streetAddress"
           placeholder="STREET ADDRESS"
@@ -20,10 +21,10 @@
         >
       </div>
       <div>
-        <input type="text" name="city" placeholder="CITY" v-model="city">
+        <input type="text" name="city" v-bind:class="{error: this.errors[4]}" placeholder="CITY" v-model="city">
       </div>
       <div>
-        <select name="provinceCode" v-model="provinceCode">
+        <select name="provinceCode" v-bind:class="{error: this.errors[5]}" v-model="provinceCode">
           <option value="NL">NL</option>
           <option value="PE">PE</option>
           <option value="NS">NS</option>
@@ -40,13 +41,13 @@
         </select>
       </div>
       <div>
-        <input type="text" name="postalCode" placeholder="POSTAL CODE" v-model="postalCode">
+        <input type="text" name="postalCode" v-bind:class="{error: this.errors[6]}" placeholder="POSTAL CODE"  v-model="postalCode">
       </div>
       <div>
-        <input type="text" name="email" placeholder="EMAIL" v-model="email">
+        <input type="text" name="email" v-bind:class="{error: this.errors[7]}" placeholder="EMAIL" v-model="email">
       </div>
       <div>
-        <button class="app_contact_btn" @click="addContact">Create Contact</button>
+        <button class="app_contact_btn" @click="addContact()">Add Contact</button>
       </div>
     </div>
   </div>
@@ -65,7 +66,8 @@ export default {
       city: "",
       provinceCode: "",
       postalCode: "",
-      email: ""
+      email: "",
+      errors: [false, false, false, false, false, false, false, false]
     };
   },
   methods: {
@@ -74,37 +76,53 @@ export default {
       var regPhone = /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/i;
       var regPostalCode = /^[A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\d$/i;
       var regEmail = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/i;
-      if (this.firstName.length <= 0){
-        alert("first name")
+      if (this.firstName.length <= 0) {
+       this.errors[0] = true;
         valid = false;
+      }else {
+        this.errors[0] = false;
       }
-      if (this.lastName.length <= 0){
-        alert("last name")
+      if (this.lastName.length <= 0) {
+        this.errors[1] = true;
         valid = false;
+      }else {
+        this.errors[1] = false;
       }
-      if (!regPhone.test(this.phoneNumber)){
-        alert("bad phone");
+      if (!regPhone.test(this.phoneNumber)) {
+        this.errors[2] = true;
         valid = false;
+      }else {
+        this.errors[2] = false;
       }
-      if (this.streetAddress.length <= 0){
-        alert("address")
+      if (this.streetAddress.length <= 0) {
+        this.errors[3] = true;
         valid = false;
+      }else {
+        this.errors[3] = false;
       }
-      if (this.city.length <= 0){
-        alert("city")
+      if (this.city.length <= 0) {
+        this.errors[4] = true;
         valid = false;
+      }else {
+        this.errors[4] = false;
       }
-      if (this.provinceCode.length <= 0){
-        alert("province")
+      if (this.provinceCode.length <= 0) {
+        this.errors[5] = true;
         valid = false;
+      }else {
+        this.errors[5] = false;
       }
-      if (!regPostalCode.test(this.postalCode)){
-        alert("bad postal code");
+      if (!regPostalCode.test(this.postalCode)) {
+        this.errors[6] = true;
         valid = false;
+      } else {
+        this.errors[6] = false;
       }
-      if (!regEmail.test(this.email)){
-        alert("bad email");
+      if (!regEmail.test(this.email)) {
+        this.errors[7] = true;
         valid = false;
+      }else {
+        this.errors[7] = false;
       }
       return valid;
     },
@@ -121,6 +139,8 @@ export default {
           email: this.email
         });
         this.$router.push({ name: "Contacts" });
+      } else {
+        this.$forceUpdate();
       }
     }    
   }
@@ -134,6 +154,30 @@ export default {
   border: 1px solid #e0dede;
   outline: none;
   font-size: 12px;
+}
+.error {
+  background: palevioletred;
+  animation: shake 0.82s cubic-bezier(.36,.07,.19,.97) both;
+  transform: translate3d(0, 0, 0);
+  backface-visibility: hidden;
+  perspective: 1000px;
+}
+@keyframes shake {
+  10%, 90% {
+    transform: translate3d(-1px, 0, 0);
+  }
+  
+  20%, 80% {
+    transform: translate3d(2px, 0, 0);
+  }
+
+  30%, 50%, 70% {
+    transform: translate3d(-4px, 0, 0);
+  }
+
+  40%, 60% {
+    transform: translate3d(4px, 0, 0);
+  }
 }
 .form div {
   margin: 20px;
